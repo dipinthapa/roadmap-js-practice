92a3032f0b5ad13f8886939c9677ed1215b4c508
unknown <dipin.thapa1@gmail.com>
2026-03-03 12:58:33 +0545

    accordion completed

---
+++ accordion/accordion.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('.faq-question').forEach(btn => {
+    btn.addEventListener('click', () => {
+      const item = btn.closest('.faq-item');
+      const isOpen = item.classList.contains('open');
+
+      document.querySelectorAll('.faq-item').forEach(el => el.classList.remove('open'));
+
+      if (!isOpen) item.classList.add('open');
+    });
+  });
